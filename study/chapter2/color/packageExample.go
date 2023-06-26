fe9c932d0f62c06391747b1d12d37e6e0d049f74
Blover <1846615034@qq.com>
2023-06-26 19:22:53 +0800

    update

--- study/chapter2/color/packageExample.go
+++
@@ -1,9 +0,0 @@
-package color
-
-type Color string
-
-const (
-	Green = "green" // 以大写字母开头，让其他包能访问成员
-	Blue  = "blue"
-	red   = "red" // 不能访问
-)
--- study/chapter2/color/packageExample_d.go
+++
@@ -1,6 +0,0 @@
-package color
-
-// 函数首字母大写，可让外部访问
-func GetColor() string {
-	 return "color" 
-}
